4a57d6a4d23599c55a90a8297af3c749e9760446
Rodion Sarygin <sarygin.rodion7@gmail.com>
2022-12-21 22:42:44 +0700

    Added game window in main.py and quit function

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from pygame_entities.game import Game
+import pygame
+
+
+RESOLUTION = (800, 640)
+FRAMERATE = 60
+VOID_COLOR = (50, 50, 50)  # Цвет фона
+
+
+def on_quit_event(event: pygame.event.Event):
+    """
+    Нужна для выхода из игры по крестику окна.
+    """
+    Game.get_instance().running = False
+
+
 def main() -> None:
-    print("Hello World!")
+    game = Game.get_instance(RESOLUTION, FRAMERATE, VOID_COLOR)
+    game.subsribe_for_event(on_quit_event, pygame.QUIT)
+    game.run()
 
 
 if __name__ == "__main__":
